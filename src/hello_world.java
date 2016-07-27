4d807e4b813ecf8b3915d1c27949bbe879523a36
gs-1261 <mansi.panpaliya@gslab.com>
2016-07-27 08:40:16 +0530

    7th commit

--- src/hello_world.java
+++ src/hello_world.java
@@ -6,7 +6,7 @@ public class hello_world {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("hello_world !! hi");
+	System.out.println("hello_world !! hi1");
 	}
 
 }
